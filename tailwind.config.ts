2a57557513e540ef6787568565fce190467f9a90
ndthang0000 <ndthang0000@gmail.com>
2024-10-08 23:42:55 +0700

    chore: fix UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
+        customGradientStart: '#763CAC',  // Coral
+        customGradientEnd: '#320F85',    // DeepPink
+        primary: 'var(--primary-color)',
       },
     },
   },
