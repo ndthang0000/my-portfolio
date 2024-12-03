08a33e64fa8e876e87756485661ba36eb05364eb
Thang Nguyen Duc 4 <thang.nguyenduc4@gameloft.com>
2024-12-03 13:59:22 +0700

    chore: complete data for project section

--- src/components/project/ProjectItem.tsx
+++ src/components/project/ProjectItem.tsx
@@ -15,20 +15,20 @@ const ProjectItem = (data: ProjectItemProps) => {
     <div className="flex flex-col lg:flex-row lg:items-center gap-4 mb-12">
       <div className="lg:w-1/2">
         <h4 className="text-primary">{data.domain}</h4>
-        <h2 className="text-2xl">{data.title}</h2>
+        <h2 className="text-2xl mb-1">{data.title}</h2>
         <div className="leading-6 shadow-inner rounded-2xl px-6 py-4 text-sm bg-[linear-gradient(266deg,rgba(105,59,147,0.4)0%,rgba(110,191,244,0.22)77%,rgba(70,144,212,0)100%)]">
           {data.description}
         </div>
         <div>
-          <div className="flex justify-center w-10 h-10 lg:w-16 lg:h-16 rounded-full">
+          <div className="flex gap-2 mt-2 justify-center lg:justify-start">
             {data.techStack.map((tech, index) => (
               <Image
                 key={index}
                 src={`/tech-skill/${tech}.png`}
-                width={24}
-                height={24}
+                width={36}
+                height={36}
                 alt={tech}
-                className="self-center w-[24px] lg:w-[36px]"
+                className="self-center w-[24px] md:w-[36px]"
               />
             ))}
           </div>
